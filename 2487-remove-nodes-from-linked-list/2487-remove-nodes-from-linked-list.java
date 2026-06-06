85a75b24ce3f323728ae30168cf5aafe6ecec96b
akashmishra185702-creator <akashmishra185702@gmail.com>
2026-06-07 00:32:52 +0530

    Time: 92 ms (5.32%), Space: 140.5 MB (11.99%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -8,33 +8,71 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+ class Solution {
     public ListNode removeNodes(ListNode head) {
-        ListNode headR = reverse(head);
-        ListNode temp = headR;
-        int max = temp.val;
-        while(temp.next!=null){
-            if(max>temp.next.val){
-                temp.next = temp.next.next;
+        ListNode temp = head;
+        Stack<Integer> st = new Stack();
+        while(temp!=null){
+            if(st.isEmpty()) st.push(temp.val);
+            else if(st.peek()>temp.val){
+                st.push(temp.val);
             }
             else {
-                max = temp.next.val;
-                temp = temp.next;
+                while(st.peek()<temp.val){
+                    st.pop();
+                    if(st.isEmpty()) break;
+                }
+                st.push(temp.val);
             }
+            temp = temp.next;
         }
-        return reverse(headR);
-        
+        ListNode answer = new ListNode(17);
+        ListNode ans = answer;
+        Stack<Integer> stack = new Stack<>();
+         while(!st.isEmpty()){
+            stack.push(st.pop());
+        }
+        while(!stack.isEmpty()){
+            temp = new ListNode(stack.pop());
+            ans.next = temp;
+            ans = temp;
+        }
+        return answer.next;
     }
-    public ListNode reverse(ListNode head){
-        ListNode currNode = head;
-        ListNode prevNode = null;
-        while(currNode!=null){
-            ListNode nextNode = currNode.next;
-            currNode.next = prevNode;
+ }
 
-            prevNode = currNode;
-            currNode = nextNode;
-        }
-        return prevNode;
-    }
-}
+
+
+
+
+
+// class Solution {
+//     public ListNode removeNodes(ListNode head) {
+//         ListNode headR = reverse(head);
+//         ListNode temp = headR;
+//         int max = temp.val;
+//         while(temp.next!=null){
+//             if(max>temp.next.val){
+//                 temp.next = temp.next.next;
+//             }
+//             else {
+//                 max = temp.next.val;
+//                 temp = temp.next;
+//             }
+//         }
+//         return reverse(headR);
+        
+//     }
+//     public ListNode reverse(ListNode head){
+//         ListNode currNode = head;
+//         ListNode prevNode = null;
+//         while(currNode!=null){
+//             ListNode nextNode = currNode.next;
+//             currNode.next = prevNode;
+
+//             prevNode = currNode;
+//             currNode = nextNode;
+//         }
+//         return prevNode;
+//     }
+// }
