71b273c35167dacf6d8ffc9df0404088535ce03f
akashmishra185702-creator <akashmishra185702@gmail.com>
2026-06-09 23:58:32 +0530

    Time: 17 ms (21.58%), Space: 48.9 MB (11.72%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,34 +1,19 @@
 class Solution {
-    public int[] nextGreaterElements(int[] nums) {
-        int n = nums.length;
+    public int[] nextGreaterElements(int[] arr) {
         Stack<Integer> st = new Stack<>();
+        int n=arr.length;
+        st.push(arr[n-1]);
         int[] ans = new int[n];
-        int max = Integer.MIN_VALUE;
-        Arrays.fill(ans,-1);
-        for(int i=n-1; i>-1; i--){
-            max = Math.max(max,nums[i]);
-            if(st.isEmpty()) st.push(nums[i]);
-            else if(st.peek()<=nums[i]){
-                while(!st.isEmpty() && st.peek()<=nums[i]){
-                    st.pop();
-                }
-                st.push(nums[i]);
-            }
-            else{
-                ans[i] = st.peek();
-                st.push(nums[i]);
-            }
+        for(int i=n-2; i>-1; i--){
+            while(st.size()>0 && st.peek()<=arr[i]) st.pop();
+            if(st.size()>0) ans[i]=st.peek();
+            st.push(arr[i]);
         }
-        for(int i=n-1; i>=0; i--){
-            while(!st.isEmpty() && st.peek() <= nums[i]){
-                st.pop();
-            }
-
-            if(ans[i] == -1 && !st.isEmpty()){
-                ans[i] = st.peek();
-            }
-
-            st.push(nums[i]);
+        for(int i=n-1; i>-1; i--){
+            while(st.size()>0 && st.peek()<=arr[i]) st.pop();
+            if(st.size()==0) ans[i]=-1;
+            else ans[i]=st.peek();
+            st.push(arr[i]);
         }
         return ans;
     }
